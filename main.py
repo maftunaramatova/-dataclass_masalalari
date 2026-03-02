e5f4f15cb3b90e59d3f258868c4df966fd739586
senyoramoon010 <djumaniyazovumarjon@gmail.com>
2026-03-02 17:40:05 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+##oddiy class
+
+# class Mashina:
+#     def __init__(self, rang,model,tezlik=60):
+#         self.rang = rang
+#         self.model = model
+#         self.tezlik = tezlik
+#
+# class Avtobus(Mashina):
+#     def __init__(self, rang,model,tezlik,orindiq_soni):
+#         super().__init__(rang,model,tezlik)
+#         self.orindiq_soni = orindiq_soni
+#
+#
+# gentra = Mashina("Qora","Gentra 1.6",150)
+# damas = Mashina("Qora","Damas 2",150)
+# bmw = Mashina("Qora","BMW M5",300)
+#
+# isuzu = Avtobus("Oq","Isuzu",500,50)
+#
+# print(isuzu.rang)
+#
+
+
+##data class
+
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Mashina:
+#     rang: str = "Oq"
+#     model : str = "Damas"
+#     tezlik: float = 60
+#
+# @dataclass
+# class Avtobus(Mashina):
+#     orindiq_soni: int = 50
+#
+# gentra = Mashina("Qora","Gentra 1.6",150)
+# Damas = Mashina("Qora","Damas 0",1000)
+# mers = Mashina("Qora","S63",200)
+#
+# isuzu = Avtobus("Oq","Isuzu",500,50)
+# print(isuzu)
+#
+#
+
+# clck.ru/3SdzuV
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
