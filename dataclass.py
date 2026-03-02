4271dcaaec7113469254d55930aba22f5340f0f7
senyoramoon010 <djumaniyazovumarjon@gmail.com>
2026-03-02 18:03:03 +0500

    Add files via upload

---
+++ dataclass.py
@@ -0,0 +1,42 @@
+##1-Masala:
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Tortburchak:
+#     eni: int
+#     boyi:int
+#
+# shakl = Tortburchak(7,9)
+# print(shakl.eni * shakl.boyi)
+
+##2-Masala:
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Doira:
+#     radyus: float
+#
+# shakl = Doira(5)
+# print(shakl.radyus * shakl.radyus * 3.14)
+
+##3-Masala:
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Talaba:
+#     ism: str
+#     baho: int
+#
+# talaba_baxosi = Talaba("Munojat",5)
+#
+# print(talaba_baxosi)
+
+
+
+
+
+
+
+
+
+
